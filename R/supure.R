4eee59544c9a8d274b5b2da3330bf46145885fa6
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2015-08-17 08:30:24 +0200

    v. 0.0.1b
    unmixing algorithm, which can use both original data and its derivative
    has been implemented

--- R/supure.R
+++ R/supure.R
@@ -12,23 +12,128 @@
 #' a width of filter used to smooth signal prior to take a derivative
 #' @param  savgol.porder
 #' a polynomial order used for smoothing
+#' @param do.norm
+#' logical, normalize the spectral data to unit area prior to unmixing or not
 #' @param  wavelength
 #' a vector with wavelength if necessary
 #' 
 #' @return
-#'  A \code{supure} object - list with several fields, including:
-#'  \item{spec}{resolved spectra of pure components.}
-#'  \item{conc}{resolved concentrations of pure components.}
+#' The method returns an object of \code{supure} class with following fields.
+#' \code{supure } object - list with several fields, including:
+#' \item{spec }{resolved spectra of pure components.}
+#' \item{conc }{resolved concentrations of pure components.}
+#' \item{purvars }{vector with indices for pure variables.}
+#' \item{purity }{vector with values for purity function.}
+#'  
 #'  
 #' @export 
-supure = function(data, ncomp, suggest = NULL, savgol.deriv = 2, 
-                  savgol.width = NULL, savgol.porder = 2, wavelength = NULL)
+supure = function(spectra, ncomp, suggest = NULL, savgol.deriv = 2, 
+                  savgol.width = NULL, savgol.porder = 2, do.norm = TRUE,
+                  wavelength = NULL)
 {
-   
-   res$call = match.call()   
-   class(res) = "supure"
-   
-   res
+  ## find pure variables 
+  purity = NULL
+  purevars = suggest
+  
+  # prepare data and take the derivative (if needed)
+  D = spectra
+  
+  # loop to locate the variables
+  # ... !!! ...
+
+  ## resolve the pure spectra and concentrations
+  
+  # get the initial estimates of concentrations
+  if (savgol.deriv > 0)
+  {  
+    deriv = -savgol(D, savgol.width, savgol.porder, savgol.deriv)
+    deriv[deriv < 0] = 0
+    Dr = deriv[, purevars, drop = F]  
+  }
+  else
+  {  
+    Dr = D[, purevars, drop = F]  
+  }
+  
+  # resolve spectra and concentrations
+  St = t(D) %*% Dr %*% solve(t(Dr) %*% Dr)
+  Ct = D %*% St %*% solve(t(St) %*% St)
+  
+  # scale
+  if (savgol.deriv > 0)
+    f = as.matrix(rowSums(deriv))
+  else
+    f = as.matrix(rowSums(D))
+ 
+  a = solve(t(Ct) %*% Ct) %*% t(Ct) %*% f
+  A = diag(as.vector(a))
+  
+  Ct = Ct %*% A
+  St = St %*% A %*% solve(t(A) %*% A)
+  
+  ## return the results
+  res = list()
+  
+  res$spec = St
+  res$conc = Ct
+  res$purity = purity
+  res$purevars = purevars
+  res$wavelength = wavelength
+  
+  res$call = match.call()   
+  class(res) = "supure"
+  res
+}
+
+#' Savytzky-Golay filter
+#' 
+#' @description
+#' Applies Savytzky-Golay filter to the rows of data matrix
+#' 
+#' @param data
+#' a matrix with data values
+#' @param width
+#' width of the filter window
+#' @param porder
+#' order of polynomial used for smoothing
+#' @param dorder
+#' order of derivative to take (0 - no derivative)
+#' 
+#' @export
+savgol = function(data, width = 3, porder = 1, dorder = 0)
+{
+  nobj = nrow(data)
+  nvar = ncol(data)
+  
+  pdata = matrix(0, ncol = nvar, nrow = nobj)
+  
+  for (i in 1:nobj)
+  {
+    d = data[i, ]
+    
+    w = (width - 1)/2                        
+    f  = pinv(outer(-w:w, 0:porder, FUN = "^"))  
+    d = convolve(d, rev(f[dorder + 1, ]), type = "o")     
+    pdata[i, ] = d[(w + 1) : (length(d) - w)] 
+  }  
+  
+  pdata
+}
+
+#' Pseudo-inverse matrix
+#' 
+#' @description
+#' Computes pseudo-inverse matrix using SVD
+#' 
+#' @param data
+#' a matrix with data values to compute inverse for
+#' 
+#' @export
+pinv = function(data)
+{
+  # Calculates pseudo-inverse of data matrix
+  s = svd(data)
+  s$v %*% diag(1/s$d) %*% t(s$u)
 }
 
 #' Overview plot for supure object
