298d5a9be207722c02057965cff88e1887375aba
Sergey Kucheryavskiy <svkucheryavski@gmail.com>
2015-09-22 12:04:34 +0200

    v. 0.0.1

---
+++ R/explore.R
@@ -0,0 +1,9 @@
+#' @export
+explore = function() {
+  appDir = system.file("shiny", "explorer", package = "supure")
+  if (appDir == "") {
+    stop("Could not find example directory. Try re-installing `supure`.", call. = FALSE)
+  }
+  
+  shiny::runApp(appDir, display.mode = "normal")
+}
